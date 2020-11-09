19c1a7b2283a406cc555cb68a137ae02794a5f5a
Wilson Styres <wstyres@gmail.com>
2020-11-09 10:42:00 -0500

    Forgot to change function name

--- kernel.c
+++ kernel.c
@@ -58,7 +58,7 @@ void create_terminal() {
     for (size_t y = 0; y < VGA_HEIGHT; y++) {
         for (size_t x = 0; x < VGA_WIDTH; x++) {
             const size_t i = y * VGA_WIDTH + x;
-            terminal_buffer[i] = make_vgachar(' ', terminal_color);
+            terminal_buffer[i] = create_vga_character(' ', terminal_color);
         }
     }
 }
