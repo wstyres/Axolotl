d0b495d93624074c4ec78ddb6b51bcfe6cff06d6
Wilson Styres <wstyres@gmail.com>
2020-11-13 18:06:54 +0000

    Initiailize PICS in the kernel

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -2,6 +2,7 @@
 #include <stdlib/stdio/stdio.h>
 #include <shell/shell.h>
 #include <stdlib/stdlib.h>
+#include <drivers/kb.h>
 
 void kernel_main() {
     create_terminal();
@@ -9,6 +10,10 @@ void kernel_main() {
     printf("Initializing Memory...\n");
     initialize_memory();
     printf("Memory Initialized!\n");
+
+    printf("Initializing PICS...\n");
+    init_pics(0x20, 0x28);
+    printf("PICS Initialized!\n");
 
 	shell();
 }
