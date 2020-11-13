b47e5a888e55c34d820c97922ae38742203ed6ee
Wilson Styres <wstyres@gmail.com>
2020-11-13 18:42:45 +0000

    Add a basic cursor

--- src/shell/shell.c
+++ src/shell/shell.c
@@ -5,23 +5,38 @@
 #include <drivers/kb.h>
 #include <stdlib/stdlib.h>
 
+void addcursor() {
+    writechar('_');
+}
+
+void removecursor() {
+    deletechar();
+}
+
 char *getinput() {
     char *input = malloc(sizeof(char) * 256);
     char c = 0;
+    addcursor();
     do {
         c = getchar();
         if (c == '\b') { // Backspace
             size_t length = strlen(input);
             if (length > 0) {
                 input[length - 1] = '\0';
+                removecursor();
                 deletechar();
+                addcursor();
             }
         }
         else {
+            removecursor();
             strccat(input, c);
             writechar(c);
+            addcursor();
         }
     } while (c != '\n');
+    removecursor();
+
     return input;
 }
 
